304580ff589471c8152a34f3299cad7a91efdbff
C0849394mehul <105397667+c0849394mehulbhai@users.noreply.github.com>
2022-05-26 20:20:54 -0400

    initial commit

--- A1_A2_iOS_Mehulbhai_C0849394/ViewController.swift
+++ A1_A2_iOS_Mehulbhai_C0849394/ViewController.swift
@@ -2,18 +2,61 @@
 //  ViewController.swift
 //  A1_A2_iOS_Mehulbhai_C0849394
 //
-//  Created by Dashmeen Kaur on 2022-05-25.
+//  .
 //
-
+import CoreLocation
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+  
+    @IBOutlet weak var mapvw: MKMapView!
+   
+    let regionInMeters: Double = 10000
+    var locationManager:CLLocationManager!
+    
+//    var arrrCity :[MKMapItem] = []
+//    var polygon: MKPolygon? = nil
+//    
+//    let item = UINavigationItem()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if (CLLocationManager.locationServicesEnabled())
+        {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+        }
     }
-
-
+    
 }
+
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last{
+            let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0))
+            self.mapvw.setRegion(region, animated: true)
+            mapvw.showsUserLocation = true
+        }
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+     
+    }
+}
+
+          
+
+ 
+
+
 
---
+++ A1_A2_iOS_Mehulbhai_C0849394/secondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  secondViewController.swift
+//  A1_A2_iOS_Mehulbhai_C0849394
+//
+//  
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+   
+}
